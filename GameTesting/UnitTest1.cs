20a41d6a9ce0a1e923d757e732430714adc9e8b1
Navpreet Kaur <NK028@atcltd.atc.org.nz>
2020-11-12 13:31:03 +1300

    Add project files.

---
+++ GameTesting/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using MyCarRace.CarRaceModule;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GameTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestFactory()
+        {
+            Punter punter = Factory.GetPunter("Olivia");
+            bool result = punter is Olivia;
+            Assert.AreEqual(result, true);
+        }
+    }
+}
